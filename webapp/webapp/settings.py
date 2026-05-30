6d6c741570d13e3428c3b1fda3f96d4def1d072e
Arvinrjb <arvinrjb13@gmail.com>
2026-05-30 19:34:42 +0330

    new update | DRF

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
     'accounts',
     'core',
     'utils',
@@ -131,3 +132,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ]
+}
--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -1,32 +1,24 @@
-"""
-URL configuration for webapp project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from system.views import ShowServer
 from accounts.views import  login_user, sign_up
 from monitoring.views import ReceiveData
+from rest_framework import routers
+# from monitoring.views import ReceiveData
 
-urlpatterns = [
+router = routers.DefaultRouter()
+# router.register(r'systems', ReceiveData)
+urlpatterns = router.urls
+
+urlpatterns += [
     path('admin/', admin.site.urls),
+    # path('', include('router.urls')),
     path('', include('system.urls')),
     path('', include('django.contrib.auth.urls')),
     path('dashboard/', include('accounts.urls')),
     path('login/', login_user.as_view()),
     path('signup/', sign_up.as_view()),
-    path('monitoringdata/', ReceiveData.as_view())
+    path('monitoringdata/', ReceiveData.as_view()),
+    # path("api-auth/", include("rest_framework.urls", namespace="rest_framework"))
     
 ]
