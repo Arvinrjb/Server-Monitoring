4ac040ce7d1b270be0d8a8e2b59c8f12a401f55a
arvinrjb <arvinrjb13@gmail.com>
2026-07-07 17:07:50 +0330

    refactor, chore (DataBase) : add PostgreSQL

--- agent/main.py
+++ agent/main.py
@@ -9,7 +9,7 @@ status_url = 'http://127.0.0.1:8000/api/agent/status/report/'
 logs_url = 'http://127.0.0.1:8000/api/agent/logs/report/'
 
 
-Token_1 = '182c25a06d6c81a2f79b34e64e13a1a10d362b62a1e87ad986a47908fe2dace2'
+Token_1 = '105958cf9e5d3f498eb18cf3c4ab9f5bda23c6b0ea096ea4fd5c6232e2d69761'
 Token_2 = 'ab8c13e8947721cf5862b1b7109cc2333f606c4b7ad3b4f5e17b15443b8b5551'
 
 
