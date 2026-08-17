48913a252d6533d8d453368b80e493ede4b9bb92
arvinrjb <arvinrjb13@gmail.com>
2026-08-17 17:47:12 +0330

    refactor: get redis and celery urls from .env

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -38,9 +38,11 @@ DEBUG = bool(os.getenv("DJANGO_DEBUG"))
 ALLOWED_HOSTS = ['*']
 
 # Celery
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/1")
 # CELERY_RESULT_BACKEND = 'django-db'
-CELERY_RESULT_BACKEND = "redis://localhost:6379/1"
+CELERY_RESULT_BACKEND = os.getenv(
+    "CELERY_RESULT_BACKEND", "redis://localhost:6379/1"
+)
 
 # Application definition
 
@@ -206,8 +208,8 @@ CACHES = {
         "BACKEND": 
             # "django.core.cache.backends.locmem.LocMemCache",
             "django_redis.cache.RedisCache",
-        "LOCATION": 
+        "LOCATION":
             # "unique-monitoring-cache",
-            "redis://127.0.0.1:6379/1",
+            os.getenv("REDIS_URL", "redis://127.0.0.1:6379/0"),
     }
 }
